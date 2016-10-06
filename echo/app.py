f6cfc298fb0caa07023eb8ca27372d6111cd7fe7
Michelle Rowley <mrowley@heroku.com>
2016-10-05 22:51:42 -0500

    Update response text, fix 'unsure' response

--- echo/app.py
+++ echo/app.py
@@ -13,12 +13,12 @@ from pylexa.response import PlainTextSpeech
 
 BASIC_RESPONSES = {
     "Dyno": "A dyno is a lightweight Linux container that runs a single user-specified command. Apps are run inside dynos on the Heroku Platform.",
-    "Addon": "Add-ons are tools and services for developing, extending, and operating your Heroku app. Some add-ons are built and maintained by Heroku while others are offered by third party vendors.",
+    "Addon": "Add-ons are tools and services for developing, extending, and operating your Heroku app. Some add-ons are built and maintained by Heroku. Others are offered by third party vendors.",
     "Connect": "Heroku Connect is an add-on that synchronizes data between your Salesforce organization and a Heroku Postgres database. Using Heroku Connect with Heroku Postgres, you can build applications that interact with your Salesforce data.",
     "Private Spaces": "Private Spaces are dedicated environments for running dynos and certain types of add-ons enclosed within an isolated network. Access to apps in a Private Space can be controlled at the network level.",
-    "Apps": "An app is a collection of source code and some dependency description that instructs Heroku as to which additional dependencies are needed in order to build and run the application.",
-    "Pipelines": "A pipeline is a group of Heroku apps that share the same codebase. Apps in a pipeline are grouped into stages representing different deployment steps in a continuous delivery workflow.",
-    "Heroku": "Heroku lets you deploy, run and manage applications written in Ruby, Node.js, Java, Python, Clojure, Scala, Go and PHP.",
+    "Apps": "An app is a collection of source code along some dependency description that tells Heroku how to build and run the application.",
+    "Pipelines": "A pipeline is a group of Heroku apps that share the same codebase. Apps in a pipeline are grouped into stages that represent different deployment steps in a continuous delivery workflow. Michelle will show you a Pipelines demo if you ask her.",
+    "Heroku": "Heroku lets you deploy, run, and manage applications written in Ruby, Node.js, Java, Python, Clojure, Scala, Go and PHP.",
     "Unsure": "You can ask me about dynos, add-ons, apps, pipelines, private spaces or Heroku connect."
 }
 
@@ -40,6 +40,7 @@ CONCEPT_RESPONSES = {
     "pipeline": BASIC_RESPONSES["Pipelines"],
     "pipelines": BASIC_RESPONSES["Pipelines"],
     "heroku": BASIC_RESPONSES["Heroku"],
+    "unsure": BASIC_RESPONSES["Unsure"],
 }
 
 app = Flask(__name__)
