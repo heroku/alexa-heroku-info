3bc02ef7e9773fb53642ce5a75fa09b9f8f2b4a1
Michelle Rowley <mrowley@heroku.com>
2016-10-05 23:25:36 -0500

    move strings to own file

--- echo/app.py
+++ echo/app.py
@@ -10,45 +10,8 @@ from pylexa.app import alexa_blueprint
 from pylexa.intent import handle_intent
 from pylexa.response import PlainTextSpeech
 
+from .strings import CONCEPT_RESPONSES
 
-BASIC_RESPONSES = {
-    "Dyno": "Apps are run inside dynos on the Heroku Platform. A dyno is a lightweight Linux container that invokes a single command to run a codebase compiled into a slug.",
-    "Slug": "Slugs are compressed and pre-packaged copies of your application. They're optimized to be run on a dyno. When you git push to Heroku, your code is received by the slug compiler which transforms your repository into a slug.",
-    "Addon": "Add-ons are tools and services for developing, extending, and operating your Heroku app. Some add-ons are built and maintained by Heroku. Others are offered by third party vendors.",
-    "Connect": "HerokuConnect is an add-on that synchronizes data between your Salesforce organization and a Heroku Postgres database. Using HerokuConnect with Heroku Postgres, you can build applications that interact with your Salesforce data.",
-    "Private Spaces": "Private Spaces are dedicated environments for running dynos and certain types of add-ons enclosed within an isolated network. Access to apps in a Private Space can be controlled at the network level.",
-    "Apps": "An app is a collection of source code along some dependency description that tells Heroku how to build and run the application.",
-    "Pipelines": "A pipeline is a group of Heroku apps that share the same codebase. Apps in a pipeline are grouped into stages that represent different deployment steps in a continuous delivery workflow. Michelle will show you a Pipelines demo if you ask her.",
-    "Heroku": "Heroku lets you deploy, run, and manage applications written in Ruby, Node.js, Java, Python, Clojure, Scala, Go and PHP.",
-    "Unsure": "You can ask Heroku about dynos, apps, add-ons, slugs, pipelines, private spaces or Herokuconnect.",
-    "Michelle": "Michelle Rawley is a Customer Solutions Architect at Heroku. She enjoys reading, yoga, and snowboarding. Her favorite color is blue.",
-    "Twitter": "Allow me to refer you to our Public Relations department...",
-}
-
-CONCEPT_RESPONSES = {
-    "dyno": BASIC_RESPONSES["Dyno"],
-    "dynos": BASIC_RESPONSES["Dyno"],
-    "dino": BASIC_RESPONSES["Dyno"],
-    "dinos": BASIC_RESPONSES["Dyno"],
-    "addon": BASIC_RESPONSES["Addon"],
-    "add-on": BASIC_RESPONSES["Addon"],
-    "addons": BASIC_RESPONSES["Addon"],
-    "add-ons": BASIC_RESPONSES["Addon"],
-    "aonnect": BASIC_RESPONSES["Connect"],
-    "heroku connect": BASIC_RESPONSES["Connect"],
-    "private space": BASIC_RESPONSES["Private Spaces"],
-    "private spaces": BASIC_RESPONSES["Private Spaces"],
-    "app": BASIC_RESPONSES["Apps"],
-    "apps": BASIC_RESPONSES["Apps"],
-    "pipeline": BASIC_RESPONSES["Pipelines"],
-    "pipelines": BASIC_RESPONSES["Pipelines"],
-    "slug": BASIC_RESPONSES["Slug"],
-    "slugs": BASIC_RESPONSES["Slug"],
-    "heroku": BASIC_RESPONSES["Heroku"],
-    "unsure": BASIC_RESPONSES["Unsure"],
-    "michelle": BASIC_RESPONSES["Michelle"],
-    "twitter": BASIC_RESPONSES["Twitter"],
-}
 
 app = Flask(__name__)
 app.config['app_id'] = os.getenv('ALEXA_APP_ID')
