89eea784157b94cc4aa27c5acd92c1740ee26a4f
Michelle Rowley <mrowley@heroku.com>
2016-10-05 22:13:09 -0500

    Testing Heroku Info responses

--- echo/app.py
+++ echo/app.py
@@ -11,9 +11,32 @@ from pylexa.intent import handle_intent
 from pylexa.response import PlainTextSpeech
 
 
+BASIC_RESPONSES = {
+    "Dyno": "Dynos are great",
+    "Addon": "Add-ons are great",
+    "Connect": "Connect is great",
+    "Private Spaces": "Spaces are great",
+    "Apps": "Apps are great",
+    "Pipelines": "Pipelines are great",
+    "Heroku": "Heroku is great",
+}
+
 CONCEPT_RESPONSES = {
-    "Dyno": "",
-    "Add-on": "",
+    "Dyno": BASIC_RESPONSES["Dyno"],
+    "Dynos": BASIC_RESPONSES["Dyno"],
+    "Addon": BASIC_RESPONSES["Addon"],
+    "Add-on": BASIC_RESPONSES["Addon"],
+    "Addons": BASIC_RESPONSES["Addon"],
+    "Add-ons": BASIC_RESPONSES["Addon"],
+    "Connect": BASIC_RESPONSES["Connect"],
+    "Heroku Connect", BASIC_RESPONSES["Connect"],
+    "Private Space": BASIC_RESPONSES["Private Spaces"],
+    "Private Spaces": BASIC_RESPONSES["Private Spaces"],
+    "App": BASIC_RESPONSES["Apps"],
+    "Apps": BASIC_RESPONSES["Apps"],
+    "Pipeline": BASIC_RESPONSES["Pipelines"],
+    "Pipelines": BASIC_RESPONSES["Pipelines"],
+    "Heroku": BASIC_RESPONSES["Heroku"],
 }
 
 app = Flask(__name__)
@@ -48,7 +71,7 @@ def handle_status_intent(request):
 
 @handle_intent('HerokuInfo')
 def handle_info_intent(request):
-    concept = request.slots.get('Concept', 'snizbag')
+    concept = request.slots.get('Concept', 'Heroku')
 
     return PlainTextSpeech(concept)
 
